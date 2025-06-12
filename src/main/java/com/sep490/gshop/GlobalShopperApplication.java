ae6d1632f4b2e5341fccbd00cbe56aa831c5eda4
ThoNT16 <truongthonguyen107@gmail.com>
2025-06-12 22:47:41 +0700

    feat: init base project [thont]

---
+++ src/main/java/com/sep490/gshop/GlobalShopperApplication.java
@@ -0,0 +1,13 @@
+package com.sep490.gshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalShopperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalShopperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sep490/gshop/GlobalShopperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sep490.gshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalShopperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
