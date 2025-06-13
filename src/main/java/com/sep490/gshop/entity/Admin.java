4cc5cd6f2540fad70eb761485f1605fb56d7c35b
ThoNT16 <truongthonguyen107@gmail.com>
2025-06-13 21:42:35 +0700

    feat: add entity admin [thont]

---
+++ src/main/java/com/sep490/gshop/entity/Admin.java
@@ -0,0 +1,18 @@
+package com.sep490.gshop.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "admins")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Admin extends User {
+    private String nation;
+}
