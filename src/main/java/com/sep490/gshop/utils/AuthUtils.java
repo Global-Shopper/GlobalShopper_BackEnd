5a5057244ed88f44f803f4acfd36bee57baa4d3e
ThoNT16 <truongthonguyen107@gmail.com>
2025-06-23 13:25:04 +0700

    feat: Adds authentication utility class [thont]
    Introduces AuthUtils to retrieve current user details, ID, and username from the security context.
    
    Handles cases where no user is authenticated.

---
+++ src/main/java/com/sep490/gshop/utils/AuthUtils.java
@@ -0,0 +1,31 @@
+package com.sep490.gshop.utils;
+
+import com.sep490.gshop.config.handler.AppException;
+import com.sep490.gshop.config.security.services.UserDetailsImpl;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.UUID;
+
+public class AuthUtils {
+    private AuthUtils() {
+    }
+    public static UserDetailsImpl getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication.getPrincipal() != "anonymousUser") {
+            return (UserDetailsImpl) authentication.getPrincipal();
+        } else {
+            throw new AppException(401,"User not found in security context");
+        }
+    }
+
+    public static UUID getCurrentUserId() {
+        UserDetailsImpl user = getCurrentUser();
+        return user.getId();
+    }
+
+    public static String getCurrentUsername() {
+        UserDetailsImpl user = getCurrentUser();
+        return user.getUsername();
+    }
+}
