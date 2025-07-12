9ea6d0a4ed5ad36c052fefe61a31d93d81b2f3ac
phamhminhkhoi <phamkhoi172003@gmail.com>
2025-07-12 15:11:25 +0700

    hotfix: change currCode [phamhminhkhoi]

--- src/main/java/com/sep490/gshop/service/implement/VNPayServiceImpl.java
+++ src/main/java/com/sep490/gshop/service/implement/VNPayServiceImpl.java
@@ -35,7 +35,7 @@ public class VNPayServiceImpl {
     public String createURL(double money, String reason, String userEmail) {
         try {
             var random = ThreadLocalRandom.current();
-            String currCode = "VNĐ";
+            String currCode = "VND";
             Map<String, String> vnpParams = new TreeMap<>();
             vnpParams.put("vnp_Version", "2.1.0");
             vnpParams.put("vnp_Command", "pay");
