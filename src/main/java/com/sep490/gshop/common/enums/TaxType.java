137d185baab8b87c3fd86ef02ef89e6532089f99
Pham Hoang Minh Khoi <phamkhoi172003@gmail.com>
2025-07-28 23:36:55 +0700

    hotfix: Has been treated with special medicine for various types of bugs, such as null or error casts [phamhminhkhoi]

--- src/main/java/com/sep490/gshop/common/enums/TaxType.java
+++ src/main/java/com/sep490/gshop/common/enums/TaxType.java
@@ -1,6 +1,6 @@
 package com.sep490.gshop.common.enums;
 
 public enum TaxType {
-    VAT, MFN, RCEPT, VJEPA, ACFTA, AJCEP, VKFTA, AKFTA, UKVFTA, TTDB
+    VAT, MFN, RCEPT, VJEPA, ACFTA, AJCEP, VKFTA, AKFTA, UKVFTA, TTDB,NHAP_KHAU_UU_DAI, NHAP_KHAU_TOI_UU
 
 }
