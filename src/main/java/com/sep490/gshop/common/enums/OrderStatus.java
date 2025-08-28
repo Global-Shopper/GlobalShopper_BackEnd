f817c6b967c019dd3e598ae40bce9013827626fd
ThoNT16 <truongthonguyen107@gmail.com>
2025-08-28 13:10:00 +0700

    fix: fix send noti and update order status [thont]

--- src/main/java/com/sep490/gshop/common/enums/OrderStatus.java
+++ src/main/java/com/sep490/gshop/common/enums/OrderStatus.java
@@ -1,11 +1,20 @@
 package com.sep490.gshop.common.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum OrderStatus {
-    AWAITING_PAYMENT,
-    ORDER_REQUESTED,
-    PURCHASED,
-    IN_TRANSIT,
-    ARRIVED_IN_DESTINATION,
-    DELIVERED,
-    CANCELLED,
+    AWAITING_PAYMENT("Đơn hàng chờ thanh toán"),
+    ORDER_REQUESTED("Đơn hàng đã được đặt"),
+    PURCHASED("Đơn hàng đã được mua"),
+    IN_TRANSIT("Đơn hàng đang được vận chuyển"),
+    ARRIVED_IN_DESTINATION("Đơn hàng đã đến nơi"),
+    DELIVERED("Đơn hàng đã được giao"),
+    CANCELLED("Đơn hàng đã bị hủy");
+
+    private final String description;
+
+    OrderStatus(String description) {
+        this.description = description;
+    }
 }
